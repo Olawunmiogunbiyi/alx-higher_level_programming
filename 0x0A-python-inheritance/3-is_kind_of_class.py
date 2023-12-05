123e846230615238204548f6eeb293a2de2e2351
olawunmiogunbiyi <olawunmiogunbiyi153@gmail.com>
2023-12-05 09:24:37 -0800

    3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,7 @@
-#bin/usr/python
+#!/usr/bin/python3
+""" check if object is instance of a class or sublass """
+
 
 def is_kind_of_class(obj, a_class):
-
-     return isinstance(obj, a_class)
+    """ checks instance of a class """
+    return isinstance(obj, a_class)
